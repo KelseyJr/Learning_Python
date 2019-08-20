4ac8c612d726470dc9e7919d83c27c611d185f1b
Kelsey Junior <kelseytfjunior@gmail.com>
2019-08-20 20:00:00 -0300

    List Functions

---
+++ listFunctions.py
@@ -0,0 +1,19 @@
+lucky_number = [4, 8, 15, 16, 23, 42]
+friends = ['Kevin', 'Karen', 'Jim', 'Jim', 'Oscar', 'Toby']
+
+#friends.extend(lucky_number)
+#friends.append('Creed')
+#friends.insert(1, 'Kelly')
+#friends.remove('Jim')
+#friends.clear()
+#friends.pop()
+
+#print(friends.index('Kevin'))
+#print(friends.count('Jim'))
+#friends.sort()
+#print(friends)
+
+#lucky_number.reverse()
+
+friends2 = friends.copy()
+print(friends2)
