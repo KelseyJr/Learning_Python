a6660e63cace17fc1f65a1bb37456e646c96c2ab
Kelsey Junior <kelseytfjunior@gmail.com>
2019-08-20 16:30:14 -0300

    Variables & Data Types

---
+++ variable.py
@@ -0,0 +1,8 @@
+character_name = 'Tom'
+character_age = 50
+is_male = True
+
+print(f'There onde was a man named {character_name},')
+print(f'he was {character_age} years old. ')
+print(f'He really liked the name {character_name}, ')
+print(f"but didn't like being {character_age}.")
